5a5339c31dbe6dc4cd3ae9c97b6390c5ee85c05d
waterbender <pask.eugene@gmail.com>
2015-07-30 09:18:01 +0300

    Initial Commit

---
+++ 46-47 Client API/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  46-47 Client API
+//
+//  Created by  ZHEKA on 30.07.15.
+//  Copyright (c) 2015 Pasko Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 46-47 Client API/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  46-47 Client API
+//
+//  Created by  ZHEKA on 30.07.15.
+//  Copyright (c) 2015 Pasko Eugene. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+

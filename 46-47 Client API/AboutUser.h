f9f42bd1c4b10e1e36e88f361a33d8ae5aed8494
waterbender <pask.eugene@gmail.com>
2015-08-01 12:18:10 +0300

    firstCommit after bad things

---
+++ 46-47 Client API/AboutUser.h
@@ -0,0 +1,15 @@
+//
+//  AboutUser.h
+//  46-47 Client API
+//
+//  Created by  ZHEKA on 01.08.15.
+//  Copyright (c) 2015 Pasko Eugene. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "ServerObject.h"
+
+@interface AboutUser : ServerObject
+
+
+@end
